1997ad31c9448f1c0ceb84aedd58562ad0197a38
hastikacheddy <59787014+hastikacheddy@users.noreply.github.com>
2022-01-05 13:15:27 +0400

    Add files via upload

---
+++ WelcomeApplication.java
@@ -0,0 +1,7 @@
+public class WelcomeApplication
+{
+public static void main(String [] args)
+{
+System.out.println("Welcome to Java!\n");
+}
+}
---
+++ labsheet1q2.java
@@ -0,0 +1,7 @@
+public class labsheet1q2 {
+  
+    public static void main(String [] args)
+{
+System.out.println("Hastika Cheddy\n20\n57770306\n16 eme mille\n");
+}
+}
